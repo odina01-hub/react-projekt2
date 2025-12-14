717f358c62c925b87fa15bbd4f5e338af46952f0
odina01-hub <axmadxojayevaodina01@gmail.com>
2025-12-14 06:45:01 +0500

    dfghj

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Teachers from "./pages/teachers/Teachers";
+import Students from "./pages/students/Students";
+import Layout from "./components/Layout";
+import TeachersSingle from "./pages/Single/TeachersSingle";
+import StudentsSingle from "./pages/StudentsSingle/StudentsSingle";
+import LoginPage from "./pages/login/LoginPage";
+import { useState } from "react";
+
+const App = () => {
+  const [isAuth, setIsAuth] = useState(localStorage.getItem("auth") || false);
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginPage setIsAuth={setIsAuth} />} />
+        <Route element={isAuth ? <Layout /> : <Navigate to="/" />}>
+          <Route path="teachers" element={<Teachers />} />
+          <Route path="teachers/:id" element={<TeachersSingle />} />
+          <Route path="students" element={<Students />} />
+          <Route path="students/:id" element={<StudentsSingle />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
